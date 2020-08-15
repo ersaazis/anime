a5cfddd70356cf523bc77875684daa5606579efd
ersaazis <eam24maret@gmail.com>
2020-08-15 19:37:56 +0700

    update episode

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -74,7 +74,7 @@
                         >
                         @if (!session('lite_mode',false))
                         <img src="{{url($item->foto)}}" class="img-fluid position-relative mx-auto"/>
-                <h3 class="w-75 position-absolute text-dark bg-white p-1" style="font-size:50%;font-weight:700;top:5%">{!! $item->judul_anime !!}</h3>
+                <h3 class="w-75 position-absolute text-dark bg-white p-1" style="font-size:50%;font-weight:700;top:5%">{!! $item->nama !!}</h3>
                 <h3 class="w-75 position-absolute text-white bg-info p-1" style="font-size:50%;font-weight:700;bottom:0%">{!! $item->judul !!}</h3>
                 <h3 class="w-30 position-absolute text-white bg-danger p-1" style="font-size:50%;font-weight:700;bottom:10%">{!! $item->status !!}</h3>
                         @else
